415c44a5cc717cc7862b03e5afa5bd465afccae7
Itisa Saw <itisasaw@Itisas-MacBook-Air.local>
2026-01-03 17:07:09 +0530

    First Commit

---
+++ src/main/java/com/kos/KosApplication.java
@@ -0,0 +1,17 @@
+package com.kos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
+public class KosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KosApplication.class, args);
+	}
+
+}
