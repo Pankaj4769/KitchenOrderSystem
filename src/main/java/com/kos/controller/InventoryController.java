ae9b2f8a757b71e7dc31811ce7d30417746188cc
Pankaj4769 <pankajraj4769@gmail.com>
2026-01-04 23:30:33 +0530

    addItem added

--- src/main/java/com/kos/controller/InventoryController.java
+++ src/main/java/com/kos/controller/InventoryController.java
@@ -1,17 +1,32 @@
 package com.kos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.kos.dto.Item;
+import com.kos.service.ItemService;
 
 @RestController
 public class InventoryController {
 	
+	@Autowired
+	ItemService itemService;
+	
 	@GetMapping("/health")
 	public ResponseEntity<String> getHealth() {
-		return new ResponseEntity<String>("OK",HttpStatus.OK);
+		return new ResponseEntity<String>("Ok",HttpStatus.OK);
+	}
+	
+	@PostMapping("/addItem")
+	public ResponseEntity<Item> addItem(@RequestBody Item item){
+		
+		return new ResponseEntity<Item>(itemService.addItem(item), HttpStatus.OK);
+		
 	}
 
 }
