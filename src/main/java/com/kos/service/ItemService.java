d6ab364f37b80a6fa2fdaae6c0438ec1747fb52a
Pankaj4769 <pankajraj4769@gmail.com>
2026-01-06 23:43:54 +0530

    added

--- src/main/java/com/kos/service/ItemService.java
+++ src/main/java/com/kos/service/ItemService.java
@@ -1,5 +1,7 @@
 package com.kos.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,11 +11,19 @@ import com.kos.repository.ItemRepository;
 @Service
 public class ItemService {
 	
+	Logger logger = LoggerFactory.getLogger(ItemService.class);
+	
 	@Autowired
 	ItemRepository itemRepository;
 	
 	public Item addItem(Item item) {
-		return itemRepository.save(item);
+		try {
+			return itemRepository.save(item);
+		}catch(Exception e){
+			logger.debug("Not able to  save item. Some Exception occurred");
+			return new Item();
+		}
+		 
 	}
 
 }
