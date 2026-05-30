9e0eb4b074a19128e0ed1b9aa66788f4f9447447
vikram52151 <vk8116771@gmail.com>
2026-05-31 01:09:00 +0530

    feat: user-scoped language column and /auth/profile/language endpoints

---
+++ src/main/java/com/kos/dto/LanguageUpdateRequest.java
@@ -0,0 +1,13 @@
+package com.kos.dto;
+
+public class LanguageUpdateRequest {
+    private String language;
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+}
